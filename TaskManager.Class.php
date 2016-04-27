a266f88b1a111598cfd25017ac9ef0d90edf5b12
Cyhzault <Cyhzault@gmail.com>
2016-04-27 18:05:58 +0200

    Added files via upload

--- TaskManager.Class.php
+++ TaskManager.Class.php
@@ -19,13 +19,13 @@ class TaskManager
     ///////////////////////////// Requests \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
-    public function getUserById($userId)
+    public function getTaskById($taskId)
     {
         //toutes les taches de l'utilisateur
-        $userId = (string) $userId;
-        $sql = "SELECT * FROM tache WHERE tache.id_utilisateur=:userId";
+        $tacheId = (string) $taskId;
+        $sql = "SELECT * FROM tache WHERE tache.id_tache=:taskId";
         $req = $this->db->prepare($sql);
-        $req->bindParam(':userId', $userId, PDO::PARAM_STR);
+        $req->bindParam(':taskId', $taskId, PDO::PARAM_INT);
         $req->execute();
         while($data = $req->fetch(PDO::FETCH_ASSOC)){
             $donnees = $data;
@@ -36,53 +36,70 @@ class TaskManager
 
 //Dans un projet donné
 
+    /**
+     * @param $userId
+     * @param $projectId
+     * @return mixed
+     */
     public function getTasksToDo($userId, $projectId)
     {
         $userId = (string) $userId;
         $projectId=(string) $projectId;
-        $sql = "SELECT * FROM tache NATURAL JOIN tache_in_sprint NATURAL JOIN sprint WHERE tache.id_utilisateur=:userId AND tache.etat=:to_do AND sprint.id_projet=:projectId AND tache.validation=TRUE ";
+        $list = array();
+        $i = 0;
+        $sql = "SELECT tache.id_tache,tache.nom,tache.desc,tache.date_deb,tache.date_fin FROM tache JOIN tache_in_sprint ON tache.id_tache = tache_in_sprint.id_tache JOIN sprint ON tache_in_sprint.id_sprint = sprint.id_sprint WHERE tache.id_utilisateur=:userId AND tache.etat='TODO' AND sprint.id_projet=:projectId AND tache.validation=TRUE ";
         $req = $this->db->prepare($sql);
         $req->bindParam(':userId', $userId, PDO::PARAM_STR);
         $req->bindParam(':projectId', $projectId, PDO::PARAM_STR);
         $req->execute();
         while($data = $req->fetch(PDO::FETCH_ASSOC)){
-            $donnees = $data;
+            $i++;
+            $donnees = new Task($data);
+            $list[$i] = $donnees;
         }
         $req->closeCursor();
-        return new User($donnees);
+        return $list;
     }
 
-    public function getTasksDoing($userId, $projectId)
+    public function getTasksInProgress($userId, $projectId)
     {
         $userId = (string) $userId;
         $projectId=(string) $projectId;
-        $sql = "SELECT * FROM tache NATURAL JOIN tache_in_sprint NATURAL JOIN sprint WHERE tache.id_utilisateur=:userId AND tache.etat=:doing AND sprint.id_projet=:projectId AND tache.validation=TRUE ";
+        $list = array();
+        $i = 0;
+        $sql = "SELECT tache.id_tache,tache.nom,tache.desc,tache.date_deb,tache.date_fin FROM tache JOIN tache_in_sprint ON tache.id_tache = tache_in_sprint.id_tache JOIN sprint ON tache_in_sprint.id_sprint = sprint.id_sprint WHERE tache.id_utilisateur=:userId AND tache.etat='INPG' AND sprint.id_projet=:projectId AND tache.validation=TRUE ";
         $req = $this->db->prepare($sql);
-        $req->bindParam(':userId', $userId, PDO::PARAM_STR);
-        $req->bindParam(':projectId', $projectId, PDO::PARAM_STR);
+        $req->bindParam(':userId', $userId, PDO::PARAM_INT);
+        $req->bindParam(':projectId', $projectId, PDO::PARAM_INT);
         $req->execute();
         while($data = $req->fetch(PDO::FETCH_ASSOC)){
-            $donnees = $data;
+            $i++;
+            $donnees = new Task($data);
+            $list[$i] = $donnees;
         }
         $req->closeCursor();
-        return new User($donnees);
+        return $list;
     }
 
 
 public function getTasksDone($userId, $projectId)
     {
         $userId = (string) $userId;
-        $projectId=(string) $projectId;
-        $sql = "SELECT * FROM tache NATURAL JOIN tache_in_sprint NATURAL JOIN sprint WHERE tache.id_utilisateur=:userId AND tache.etat=:done AND sprint.id_projet=:projectId AND tache.validation=TRUE ";
+        $projectId= (string) $projectId;
+        $list = array();
+        $sql = "SELECT tache.id_tache,tache.nom,tache.desc,tache.date_deb,tache.date_fin FROM tache JOIN tache_in_sprint ON tache.id_tache = tache_in_sprint.id_tache JOIN sprint ON tache_in_sprint.id_sprint = sprint.id_sprint WHERE tache.id_utilisateur=:userId AND tache.etat='DONE' AND sprint.id_projet=:projectId AND tache.validation=TRUE ";
         $req = $this->db->prepare($sql);
-        $req->bindParam(':userId', $userId, PDO::PARAM_STR);
-        $req->bindParam(':projectId', $projectId, PDO::PARAM_STR);
+        $req->bindParam(':userId', $userId, PDO::PARAM_INT);
+        $req->bindParam(':projectId', $projectId, PDO::PARAM_INT);
         $req->execute();
+        $i = 0;
         while($data = $req->fetch(PDO::FETCH_ASSOC)){
-            $donnees = $data;
+            $i++;
+            $donnees = new Task($data);
+            $list[$i] = $donnees;
         }
         $req->closeCursor();
-        return new User($donnees);
+        return $list;
     }
 
 
